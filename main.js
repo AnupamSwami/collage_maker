313a3310bb1e305e10e15cf12241e714842d2d0b
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-11-08 17:59:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,8 +13,7 @@ recognition.onresult = function(event) {
     var content = event.results[0][0].transcript ;
     console.log(content) ;
 
-    document.getElementById("textbox").innerHTML = content ;
-    if(content=="take my selfie") {
+    if(content=="selfie") {
         console.log("taking selfie ----") ;
        speak() ;
 } }
@@ -32,75 +31,47 @@ function speak(){
 
     
     var synth = window.speechSynthesis;
+    Webcam.attach(camera);
     speak_data = "Taking your Selfie in 5 seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
-    Webcam.attach(camera);
 
-    setTimeout(function () {
+    setTimeout(function() {
+        img_id = "selfie1" ;
         take_snapshot();
-    }, 5000)
+        speak_data = "Taking Your Next Selfie in 10 Seconds" ;
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }, 5000) ;
+
+    setTimeout(function() {
+        img_id = "selfie2" ;
+        take_snapshot();
+        speak_data = "Taking Your Next Selfie in 15 Seconds" ;
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }, 10000) ;
+
+    setTimeout(function() {
+        img_id = "selfie3" ;
+        take_snapshot();
+    }, 15000) ;
 }
 
-var img_id = "selfie1" ;
-
 function take_snapshot() {
-    var img_id = "selfie1" ;
     console.log(img_id) ;
     Webcam.snap(function(data_uri){
         if(img_id=="selfie1"){
             document.getElementById("result1").innerHTML = '<img id="selfie1" src="'+ data_uri +'">' ;
-            img_id = "selfie2" ;
-
-            var synth = window.speechSynthesis;
-            speak_data = "Taking your next Selfie in 5 seconds";
-            var utterThis = new SpeechSynthesisUtterance(speak_data);
-            synth.speak(utterThis);
-
-            setTimeout(function () {
-                save1();
-            }, 5000)
         }
 
         if(img_id=="selfie2"){
             document.getElementById("result2").innerHTML = '<img id="selfie2" src="'+ data_uri +'">' ;
-            img_id = "selfie3" ;
-
-            var synth = window.speechSynthesis;
-            speak_data = "Taking your next Selfie in 5 seconds";
-            var utterThis = new SpeechSynthesisUtterance(speak_data);
-            synth.speak(utterThis);
-
-            setTimeout(function () {
-                save2();
-            }, 5000)
             
         }
 
         if(img_id=="selfie3"){
             document.getElementById("result3").innerHTML = '<img id="selfie3" src="'+ data_uri +'">' ;
-            save3() ;
         }
     }) ;
-}
-
-function save1() {
-    link1 = document.getElementById("link1");
-    image_src1 = document.getElementById("selfie1").src;
-    link1.href = image_src1;
-    link1.click();
-}
-
-function save2() {
-    link2 = document.getElementById("link2");
-    image_src2 = document.getElementById("selfie2").src;
-    link2.href = image_src2;
-    link2.click();
-}
-
-function save3() {
-    link3 = document.getElementById("link3");
-    image_src3 = document.getElementById("selfie3").src;
-    link3.href = image_src3;
-    link3.click();
 }
